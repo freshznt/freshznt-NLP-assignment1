76749afb142e91350484c44de037406ad8cf6b56
freshznt <166628127+freshznt@users.noreply.github.com>
2024-04-10 23:01:00 +0800

    Add files via upload

--- entropy.py
+++ entropy.py
@@ -5,21 +5,18 @@ import re
 import math
 
 def group_n_adjacent(generator, n):
-    words = [next(generator) for _ in range(n)]  # 获取前 n 个词
+    words = [next(generator) for _ in range(n)]
     while True:
-        yield tuple(words)  # 返回前 n 个词组成的元组
-        words.pop(0)  # 弹出第一个词
+        yield tuple(words) 
+        words.pop(0)
         try:
-            words.append(next(generator))  # 添加下一个词
+            words.append(next(generator))
         except StopIteration:
             break
 
-
-# 读取所有的 txt 文件作为语料库
-corpus_dir = './corpus_utf8/'  # 假设所有的 txt 文件都存放在 corpus 文件夹中
+corpus_dir = './corpus_utf8/'
 corpus_files = os.listdir(corpus_dir)
 
-# 分词并统计汉字频次
 chinese_counter = Counter()
 n = 3
 
@@ -27,20 +24,17 @@ for file_name in corpus_files:
     if file_name.endswith('.txt'):
         with open(os.path.join(corpus_dir, file_name), 'r', encoding='utf-8') as file:
             text = file.read()
-            # 使用正则表达式去除非汉字字符和标点符号
             text = re.sub(r'[^\u4e00-\u9fa5]', '', text)
-            # 使用jieba分词
+            # chinese word entropy
             # words = list(jieba.cut(text))
+            # chinese character entropy
             words = list(text)
             grouped_words = group_n_adjacent(iter(words), n)
-            # 统计汉字频次
             chinese_counter.update(grouped_words)
 
-# 计算汉字的频率分布
 total_count = sum(chinese_counter.values())
 probabilities = {char: count / total_count for char, count in chinese_counter.items()}
 
-# 计算汉字的熵值
 entropy = -sum(p * math.log2(p) for p in probabilities.values())/n
 
-print("汉字熵值:", entropy)
+print("entropy:", entropy)
--- process_corpus.py
+++ process_corpus.py
@@ -4,15 +4,13 @@ import jieba.analyse
 import re
 from collections import Counter
 
-# 定义一个函数，用于读取并分词处理单个txt文件
 def process_file(file_path):
     with open(file_path, 'r', encoding='utf-8') as file:
         text = file.read()
         text = re.sub(r'[^\u4e00-\u9fa5]', '', text)
-        seg_list = jieba.cut(text, cut_all=False)  # 使用jieba进行分词
-        return list(seg_list)  # 返回分词结果，以列表形式返回
+        seg_list = jieba.cut(text, cut_all=False) 
+        return list(seg_list)
 
-# 定义一个函数，用于处理整个文件夹下的所有txt文件
 def process_folder(folder_path):
     seg_results = []
     for filename in os.listdir(folder_path):
@@ -21,43 +19,32 @@ def process_folder(folder_path):
             seg_results.extend(process_file(file_path))
     return seg_results
 
-# 设置当前文件夹路径
 folder_path = './corpus_utf8/'
 
-# 调用函数处理整个文件夹下的txt文件
 seg_results = process_folder(folder_path)
 
-# 将分词结果写入新的文件
 with open('segmented_texts.txt', 'w',encoding='utf-8') as output_file:
     output_file.write(' '.join(seg_results))
 
-# 统计分词频率并排序
 word_count = Counter(seg_results)
 sorted_word_count = sorted(word_count.items(), key=lambda x: x[1], reverse=True)
 
-# 将分词频率及排名写入文件
 with open('word_frequency.txt', 'w', encoding='utf-8') as freq_file:
     for idx, (word, freq) in enumerate(sorted_word_count, start=1):
         freq_file.write(f'{idx}. {word}: {freq}\n')
 
 
-# 加载jieba的默认停用词列表
-jieba.setLogLevel(20)  # 设置日志级别，避免显示加载停用词信息
-jieba.analyse.set_stop_words("cn_stopwords.txt")  # 停用词列表文件需要提前准备好，命名为stopwords.txt
+jieba.setLogLevel(20) 
+jieba.analyse.set_stop_words("cn_stopwords.txt")
 
-# 读取存储了分词结果的文件
 with open("word_frequency.txt", 'r', encoding='utf-8') as file:
-    seg_results = {line.split()[1][:-1]:line.split()[2] for line in file}  # 假设文件每行为 '词语: 频率'，这里只取词语部分
+    seg_results = {line.split()[1][:-1]:line.split()[2] for line in file}
 
 with open("cn_stopwords.txt", 'r', encoding='utf-8') as f:
     stopwords = set([line.strip() for line in f])
 
-# 根据jieba的停用词列表，过滤掉分词结果中的停用词
 filtered_seg_results = [(word,freq) for word,freq in seg_results.items() if word not in stopwords]
 
-# 统计过滤后的分词结果的词频
-
-# 将过滤后的分词频率保存到新的文件
 with open('filtered_word_frequency.txt', 'w', encoding='utf-8') as output_file:
     for word, freq in filtered_seg_results:
         output_file.write(f"{word}: {freq}\n")
